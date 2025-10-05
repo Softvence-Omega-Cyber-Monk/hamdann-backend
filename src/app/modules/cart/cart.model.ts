529955d0ed24c567a26f835295094452f3ebdfcd
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2025-10-05 12:29:01 +0600

    Auto commit: Added src/app/modules/cart/cart.controller.ts, Modified src/app/modules/cart/cart.interface.ts, Added src/app/modules/cart/cart.model.ts, Added src/app/modules/cart/cart.route.ts, Added src/app/modules/cart/cart.service.ts

---
+++ src/app/modules/cart/cart.model.ts
@@ -0,0 +1,29 @@
+import { Schema, model, Document } from "mongoose";
+import { ICartItem } from "./cart.interface";
+
+const CartItemSchema = new Schema<ICartItem>(
+  {
+    productId: { type: Schema.Types.ObjectId, ref: "Product", required: true },
+    price: { type: Number, required: true },
+    quantity: { type: Number, required: true },
+    image: { type: String },
+  },
+  { _id: false }
+);
+
+export interface ICart extends Document {
+  userId: Schema.Types.ObjectId;
+  items: ICartItem[]; // Array of cart items
+  subTotal: number;
+}
+
+const CartSchema = new Schema<ICart>(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: "user", required: true },
+    items: { type: [CartItemSchema], default: [] }, // Array of CartItems
+    subTotal: { type: Number, required: true, default: 0 },
+  },
+  { timestamps: true }
+);
+
+export const Cart = model<ICart>("Cart", CartSchema);
