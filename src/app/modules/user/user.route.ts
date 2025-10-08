f3429a36a904c6a457db3b37f8b772934aa9fea7
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2025-10-08 16:17:45 +0600

    update

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -2,6 +2,7 @@ import { Router, Request, Response, NextFunction } from "express";
 import { user_controllers } from "./user.controller";
 import { create_user, update_user } from "./user.validation";
 import { z } from "zod";
+import auth from "../../middlewares/auth";
 
 const userRoute = Router();
 
@@ -21,10 +22,12 @@ const validate =
 userRoute.post("/create", validate(create_user), user_controllers.create_user);
 
 // Get single user by ID
-userRoute.get("/get-single/:id", user_controllers.get_single_user);
+userRoute.get("/get-single/:id",  user_controllers.get_single_user);
 
 // Get all users
 userRoute.get("/getAll", user_controllers.get_all_users);
+userRoute.get("/myProfile", auth('Admin','Buyer','Seller'), user_controllers.myProfile);
+
 
 // Update user
 userRoute.put(
