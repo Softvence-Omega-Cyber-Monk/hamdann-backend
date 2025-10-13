e09d87baee591139ca6c8c33009f52451d173868
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2025-10-13 16:05:23 +0600

    uda

--- dist/app/configs/firebaseAdmin.js
+++ dist/app/configs/firebaseAdmin.js
@@ -1,20 +1,17 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.db = exports.messaging = void 0;
-const firebase_admin_1 = __importDefault(require("firebase-admin"));
-const path_1 = __importDefault(require("path"));
-const fs_1 = __importDefault(require("fs"));
-// If using CommonJS (tsc default module), __dirname exists
-const serviceAccountPath = path_1.default.join(__dirname, "serviceAccountKey.json");
-const serviceAccount = JSON.parse(fs_1.default.readFileSync(serviceAccountPath, "utf8"));
-if (!firebase_admin_1.default.apps.length) {
-    firebase_admin_1.default.initializeApp({
-        credential: firebase_admin_1.default.credential.cert(serviceAccount),
-    });
-}
-exports.messaging = firebase_admin_1.default.messaging();
-exports.db = firebase_admin_1.default.firestore();
-exports.default = firebase_admin_1.default;
+// import admin from "firebase-admin";
+// import path from "path";
+// import fs from "fs";
+// // If using CommonJS (tsc default module), __dirname exists
+// const serviceAccountPath = path.join(__dirname, "serviceAccountKey.json");
+// const serviceAccount = JSON.parse(
+//   fs.readFileSync(serviceAccountPath, "utf8")
+// ) as admin.ServiceAccount;
+// if (!admin.apps.length) {
+//   admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//   });
+// }
+// export const messaging = admin.messaging();
+// export const db = admin.firestore();
+// export default admin;
