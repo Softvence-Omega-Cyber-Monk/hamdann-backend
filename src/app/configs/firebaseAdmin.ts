02122e7aef8bb129dc99b0d3a4a2c47cc32c6a5d
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2025-10-13 16:03:26 +0600

    uda

--- src/app/configs/firebaseAdmin.ts
+++ src/app/configs/firebaseAdmin.ts
@@ -1,20 +1,20 @@
-import admin from "firebase-admin";
-import path from "path";
-import fs from "fs";
+// import admin from "firebase-admin";
+// import path from "path";
+// import fs from "fs";
 
-// If using CommonJS (tsc default module), __dirname exists
-const serviceAccountPath = path.join(__dirname, "serviceAccountKey.json");
+// // If using CommonJS (tsc default module), __dirname exists
+// const serviceAccountPath = path.join(__dirname, "serviceAccountKey.json");
 
-const serviceAccount = JSON.parse(
-  fs.readFileSync(serviceAccountPath, "utf8")
-) as admin.ServiceAccount;
+// const serviceAccount = JSON.parse(
+//   fs.readFileSync(serviceAccountPath, "utf8")
+// ) as admin.ServiceAccount;
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
-}
+// if (!admin.apps.length) {
+//   admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//   });
+// }
 
-export const messaging = admin.messaging();
-export const db = admin.firestore();
-export default admin;
+// export const messaging = admin.messaging();
+// export const db = admin.firestore();
+// export default admin;
