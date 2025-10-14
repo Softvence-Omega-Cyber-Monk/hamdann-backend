d9bba3d13d53c613ec1144751ccd09d394b2673f
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2025-10-14 10:55:59 +0600

    Auto commit: Modified src/app/configs/firebaseAdmin.ts

--- src/app/configs/firebaseAdmin.ts
+++ src/app/configs/firebaseAdmin.ts
@@ -4,7 +4,11 @@ if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
   throw new Error("🔥 FIREBASE_SERVICE_ACCOUNT env variable is missing");
 }
 
+// Parse JSON from env
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT!);
+
+// Fix the private_key line breaks
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 if (!admin.apps.length) {
   admin.initializeApp({
