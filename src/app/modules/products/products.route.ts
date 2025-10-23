f15db5733148eb293c449bfd777847d958e5f0c1
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2025-10-23 17:34:05 +0600

    udpae

--- src/app/modules/products/products.route.ts
+++ src/app/modules/products/products.route.ts
@@ -6,17 +6,21 @@ import { checkUserSubscription } from "../../middlewares/subscriptionCheck";
 
 const router = express.Router();
 
-router.post("/create", upload.any(), checkUserSubscription, productController.createProduct); // ✅ Create product
+router.post(
+  "/create",
+  upload.any(),
+  checkUserSubscription,
+  productController.createProduct
+); // ✅ Create product
 // router.put("/update/:id", productController.updateProduct); // ✅ Update product
 router.put(
   "/update/:id",
   upload.fields([
-    { name: 'productImages', maxCount: 10 },
-    { name: 'mainImage', maxCount: 1 }
+    { name: "productImages", maxCount: 10 },
+    { name: "mainImage", maxCount: 1 },
   ]),
   productController.updateProduct
 );
-
 
 // ✅ Get all products
 router.get("/getSingle/:id", productController.getSingleProduct); // ✅ Get single product
@@ -28,21 +32,33 @@ router.get(
 );
 router.get("/getNewArrivals", productController.getNewArrivalsProductsService);
 router.get("/getBestSelling", productController.getBestSellingProductsService);
-router.get("/getSellerBestSelling/:userId", productController.getSellerBestSellingProductsController);
-
-
+router.get(
+  "/getSellerBestSelling/:userId",
+  productController.getSellerBestSellingProductsController
+);
 
 router.put("/addReview/:productId", productController.addReviewToProduct);
-router.get("/stats/:userId", productController.getProductStats); 
+router.get("/stats/:userId", productController.getProductStats);
 
 // Inventory status
 router.get("/inventory-status/:userId", productController.getInventoryStatus);
 
 // Single product inventory status route
 router.get("/inventory/:productId", productController.handleGetInventoryStatus);
-router.patch("/updateQuantity/:productId", productController.handleUpdateQuantity);
+router.patch(
+  "/updateQuantity/:productId",
+  productController.handleUpdateQuantity
+);
 
 // Single product statistics route
-router.get("/product-stats/:productId", productController.handleGetSingleProductStats);
+router.get(
+  "/product-stats/:productId",
+  productController.handleGetSingleProductStats
+);
+
+router.get(
+  "/sales-trends/:sellerId",
+  productController.getSalesTrendsController
+);
 
 export const productRoutes = router;
